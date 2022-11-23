820b73cbdc5bf4c9d7d5b2364df14d9b4a4aca4e
User <User@DESKTOP-BLCPVF5>
2022-11-22 20:39:40 -0400

    Modificacion a Bolsa de Empleo , con la creaciob del crud de empleado y de persona

--- Proyecto_Final/src/Logico/BolsaEmpleo.java
+++ Proyecto_Final/src/Logico/BolsaEmpleo.java
@@ -22,4 +22,46 @@ public class BolsaEmpleo {
 	public ArrayList<Empresa> getMisEmpleos() {
 		return misEmpleos;
 	}
+	
+	public void crearPersona(Persona p1) {
+		misPersonas.add(p1);
+	}
+	
+	public void crearEmpresa(Empresa emp) {
+		misEmpleos.add(emp);
+	}
+
+	public Persona buscarPersona(String cedula) {
+		int ind = 0;
+		while(ind < misPersonas.size() && misPersonas.get(ind).getCedula().equalsIgnoreCase(cedula)) {
+			ind++;
+		}
+		return misPersonas.get(ind);
+	}
+	
+	public Empresa buscarEmpresa(String rcn) {
+		int ind = 0;
+		while(ind < misEmpleos.size() && misEmpleos.get(ind).getRcn().equalsIgnoreCase(rcn)) {
+			ind++;
+		}
+		return misEmpleos.get(ind);
+	}
+	
+	public void eliminarEmpresa(String rcn) {
+		int ind = 0;
+		while(ind < misEmpleos.size() && misEmpleos.get(ind).getRcn().equalsIgnoreCase(rcn)) {
+			ind++;
+		}
+		misEmpleos.remove(ind);
+	}
+	
+	public void eliminarPersona(String cedula) {
+		int ind = 0;
+		while(ind < misPersonas.size() && misPersonas.get(ind).getCedula().equalsIgnoreCase(cedula)) {
+			ind++;
+		}
+		misPersonas.remove(ind);
+	}
+	
+	
 }
